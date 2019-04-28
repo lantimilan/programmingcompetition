50ff21b06c0379a162be5782e927162987dfdf20
Li Yan <lantimilan@gmail.com>
2019-04-27 23:26:15 -0700

    Update comments and complete impl on array_rotation.

--- algorithms/array_rotation.cc
+++ algorithms/array_rotation.cc
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+// Algorithm:
+// Let A and B be the two partitions
+// if A is shorter, then split B into Bl and Br, and swap(A, Br), then recurse
+// on BrBl
+// else B is shorter, then split A into Al and Ar, and swap(Al, B), then
+// recurse on ArAl
+// Other O(n) rotation algorithms:
+// 1. Let g = gcd(n, d)
+// then rotate a[i], a[i+g], a[i+2g], ..., for i = 0 to g-1
+// 2. Reverse A, Reverse B, Reverse a[]
 void rorate(int a[], int n, int d) {
     int b = 0, e = n, m = d;
     while (b < m) {
@@ -9,7 +19,7 @@ void rorate(int a[], int n, int d) {
         for (int i = 0; i < l; ++i) {
             swap(a[b+i], a[m+i]);
         }
-        
+
         if (l1 < l2) e -= l;
         else b += l;
     }
